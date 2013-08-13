694cf45ac2f4889422e32222b520c1cd6b2cc02d
Evgeniy Plyusnin <eninbox@gmail.com>
2013-08-13 11:49:41 +0600

    Поддержка null-значений в LingBooleanConverter

--- Converters/Linguistics/LingBooleanConverter.cs
+++ Converters/Linguistics/LingBooleanConverter.cs
@@ -13,20 +13,13 @@ namespace Converters
     public class LingBooleanConverter : ConverterBase<LingBooleanConverter>
     {
         public String TrueText { get; set; }
-        private String _ft;
-
-        public String FalseText
-        {
-            get { return _ft; }
-            set
-            {
-                _ft = value;
-            }
-        }
+        public String FalseText { get; set; }
+        public String NullText { get; set; }
         
 
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            if (value == null) return NullText;
             if (!(value is Boolean)) throw new ArgumentException("Параметр должен быть типа Boolean");
             return (Boolean)value ? TrueText : FalseText;
         }
@@ -35,6 +28,7 @@ namespace Converters
         {
             TrueText = "Да";
             FalseText = "Нет";
+            NullText = "--";
         }
     }
 }
