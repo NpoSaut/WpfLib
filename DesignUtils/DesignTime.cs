c09a7428bb632a31421fd67dac4e7450f4a547de
Plyusnin Evgeniy <eninbox@gmail.com>
2020-05-22 20:10:08 +0500

    Перевёл на .Net Standard 2.0, убрал лишние ссылки

--- DesignUtils/DesignTime.cs
+++ DesignUtils/DesignTime.cs
@@ -10,7 +10,8 @@ namespace DesignUtils
     public static class DesignTime
     {
         public static readonly DependencyProperty BackgroundProperty = DependencyProperty.RegisterAttached(
-            "Background", typeof (Brush), typeof (DesignTime), new PropertyMetadata(default(Brush), SetBackgroundCallback));
+            "Background", typeof(Brush), typeof(DesignTime),
+            new PropertyMetadata(default(Brush), SetBackgroundCallback));
 
         private static bool? _inDesignMode;
 
@@ -21,10 +22,13 @@ namespace DesignUtils
                 if (_inDesignMode == null)
                 {
                     DependencyProperty prop = DesignerProperties.IsInDesignModeProperty;
-                    _inDesignMode = (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof (FrameworkElement)).Metadata.DefaultValue;
+                    _inDesignMode = (bool) DependencyPropertyDescriptor
+                                          .FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
 
                     if (!_inDesignMode.GetValueOrDefault(false) && Process.GetCurrentProcess()
-                                                                          .ProcessName.StartsWith("devenv", StringComparison.Ordinal))
+                                                                          .ProcessName
+                                                                          .StartsWith(
+                                                                               "devenv", StringComparison.Ordinal))
                         _inDesignMode = true;
                 }
 
@@ -32,14 +36,21 @@ namespace DesignUtils
             }
         }
 
-        private static void SetBackgroundCallback(DependencyObject O, DependencyPropertyChangedEventArgs PropertyChangedEventArgs)
+        private static void SetBackgroundCallback(
+            DependencyObject O, DependencyPropertyChangedEventArgs PropertyChangedEventArgs)
         {
             if (!InDesignMode) return;
             O.SetValue(Control.BackgroundProperty, PropertyChangedEventArgs.NewValue);
         }
 
-        public static void SetBackground(DependencyObject element, Brush value) { element.SetValue(BackgroundProperty, value); }
+        public static void SetBackground(DependencyObject element, Brush value)
+        {
+            element.SetValue(BackgroundProperty, value);
+        }
 
-        public static Brush GetBackground(DependencyObject element) { return (Brush)element.GetValue(BackgroundProperty); }
+        public static Brush GetBackground(DependencyObject element)
+        {
+            return (Brush) element.GetValue(BackgroundProperty);
+        }
     }
 }
